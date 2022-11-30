1f03708a94549831d44ae064f4999f4eca0af060
wtwente20 <100497007+wtwente20@users.noreply.github.com>
2022-11-29 21:06:03 -0600

    updated for more specific autograde criteria

--- scriptHelper.js
+++ scriptHelper.js
@@ -46,27 +46,27 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
       } else {
         list.style.visibility = "visible";
         pilotStatus.innerHTML = `Pilot ${pilot} is ready for launch`;
-        copilotStatus.innerHTML = `Copilot ${copilot} is ready for launch`;
+        copilotStatus.innerHTML = `Co-pilot ${copilot} is ready for launch`;
         if (fuelLevel < 10000 && cargoLevel <= 10000) {
           fuelStatus.innerHTML = "Fuel level too low for launch";
           cargoStatus.innerHTML = "Cargo mass low enough for launch";
           launchStatus.innerHTML = "Shuttle Not Ready for Launch";
-          launchStatus.style.color = "red"
+          launchStatus.style.color = "rgb(199, 37, 78)"
         } else if (fuelLevel >= 10000 && cargoLevel > 10000) {
           fuelStatus.innerHTML = "Fuel level high enough for launch";
           cargoStatus.innerHTML = "Cargo mass too heavy for launch";
           launchStatus.innerHTML = "Shuttle Not Ready for Launch";
-          launchStatus.style.color = "red"
+          launchStatus.style.color = "rgb(199, 37, 78)"
         } else if (fuelLevel < 10000 && cargoLevel > 10000) {
           fuelStatus.innerHTML = "Fuel level too low for launch";
           cargoStatus.innerHTML = "Cargo mass too heavy for launch";
           launchStatus.innerHTML = "Shuttle Not Ready for Launch";
-          launchStatus.style.color = "red"
+          launchStatus.style.color = "rgb(199, 37, 78)"
         } else {
           fuelStatus.innerHTML = "Fuel level high enough for launch";
           cargoStatus.innerHTML = "Cargo mass low enough for launch";
           launchStatus.innerHTML = "Shuttle is Ready for Launch";
-          launchStatus.style.color = "green"
+          launchStatus.style.color = "rgb(65, 159, 106)"
         }
      }
 
